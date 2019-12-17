cdaa991903f37f866dd041231dc711467ff5a451
xyh1 <xyh@example.com>
2019-12-17 16:34:54 +0800

    add truncate api test

--- loongoopBench/api/sbin/testApi.sh
+++ loongoopBench/api/sbin/testApi.sh
@@ -5,6 +5,8 @@ path=`cd $path;cd "..";pwd`
 . "${path}/bin/commands"
 . "${path}/bin/common"
 caseConfDir="$path/conf/caseConf"
+
+
 mountPath=$(getMountPath)
 runCase(){
  exist=$(echo "$1"| grep "#")
@@ -15,7 +17,7 @@ runCase(){
     echo "$apiConf are prefered!"
    else
      #echo "2:$2"
-     for caseName in $2
+     for caseName in $cases
      do
       passed=$(echo $caseName | grep "#")
       if [ "$passed" != "" ]; then
@@ -33,17 +35,29 @@ runCase(){
  fi
 }
 
+echo_help(){
+	echo "usage:"
+	echo " <apiName> [caseName]"
+}
+
 if [ 0 -eq $# ]; then
- echo "usage:"
- echo " <apiName> [caseName]"
-elif [ 1 -eq $# ]; then
- apiName=$1
- . "${path}/bin/$apiName/functions"
- cases=`ls "$path/bin/$apiName/case"`
+	echo_help
+elif [ 1 -eq $# -o 2 -eq $# ]; then
+	apiName=$1              # apiName 
+	if [ ! -d $path/bin/"$apiName" ]; then
+    	echo "apiName are prefered!"
+    	echo "check $path/bin/"$apiName" exist "
+		echo_help
+    	exit 1
+	fi
+
+	cases=$2                # 测试的cases
+	if [ "$cases" == "" ];then
+    	cases=`cat $caseConfDir/$apiName`       #调用testApi时用到
+	fi
+	 . "${path}/bin/$apiName/functions"
 else
- apiName=$1
- . "${path}/bin/$apiName/functions"
- cases=$2
+	echo_help
 fi
 if [ "" == "$rootPath" ] || [ "" == "$tmpDir" ]; then
  echo "rootPath and tmpDir are not set!"
